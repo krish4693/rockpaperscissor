9dae2074cc85868b3b35a32a47ebc5a019dc1ab8
krish <krishnanandunnikrishnan@gmail.com>
2023-03-31 22:12:54 +0530

    Completed index.html till footer(button style remaining)

---
+++ index.js
@@ -0,0 +1,28 @@
+let playerScore = 0
+let computerScore = 0
+let roundWinner = ''
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        roundWinner = 'tie'
+    }
+    if ((playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+        (playerSelection === 'PAPER' && computerSelection === 'ROCK')) {
+        playerScore++
+        roundWinner = 'player'
+    }
+    if ((
+        (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
+        (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
+        (computerSelection === 'PAPER' && playerSelection === 'ROCK')
+    )) {
+        computerScore++
+        roundWinner = 'computer'
+    }
+
+}
+
+const computerSelection=getRandomChoice()
+playRound(playerSelection,computerSelection)
+
