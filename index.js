43c9c872544a4fd52cedca82071071596fbfdbbd
krish <krishnanandunnikrishnan@gmail.com>
2023-04-01 07:21:55 +0530

    updateChoice function completed

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ let playerScore = 0
 let computerScore = 0
 let roundWinner = ''
 
+const playerSign = document.getElementById('playerSign')
+const computerSign=document.getElementById('sign')
+
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         roundWinner = 'tie'
@@ -23,6 +27,39 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-const computerSelection=getRandomChoice()
-playRound(playerSelection,computerSelection)
+function updateChoices(playerSelection, computerScore) {
+    switch (playerSelection) {
+        case 'ROCK':
+            playerSign.textContent = '✊'
+            break
+        case 'PAPER':
+            playerSign.textContent = '✋'
+        case 'SCISSOR':
+            playerSign.textContent = '✌'
+            break
+    }
+
+
+  switch (computerSelection) {
+    case 'ROCK':
+      computerSign.textContent = '✊'
+      break
+    case 'PAPER':
+      computerSign.textContent = '✋'
+      break
+    case 'SCISSORS':
+      computerSign.textContent = '✌'
+      break
+  }
+}
+
+function updateScore(){
+    if(roundWinner==='tie')
+
+
+
+const computerSelection = getRandomChoice()
+playRound(playerSelection, computerSelection)
+
+
 
